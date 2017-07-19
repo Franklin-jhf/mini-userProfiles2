eb165ec0153a9209d7f94fffa0229867b1d33d11
Franklin Floresca <franklin.jhf@gmail.com>
2017-07-19 12:54:32 -0500

    followed along with samples

--- controller.js
+++ controller.js
@@ -1,10 +1,34 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	$scope.users = mainService.getUsers().then(function(response) {
+  		$scope.users = response.data.data;
+  	});
   }
 
 
   $scope.getUsers();
 
+
+
+  $scope.addUser = function(user) {
+  	mainService.addUser(user).then(function(response) {
+  		if (response.status === 200) {
+  			$scope.alert = 'User Added';
+  		} else {
+  			$scope.alert = 'Error in Request';
+  		}
+  	});
+  }
+
+  $scope.removeFriend = function(user) {
+  	mainService.removeFriend(user).then(function(response) {
+		if (!response) {
+			$scope.alert = 'unavailable';
+		} else if (response.err) {
+			$console.log(resposne.err);
+			$scope.alert = response.err;
+		}
+  	})
+  }
 });
--- service.js
+++ service.js
@@ -1,29 +1,30 @@
 angular.module('userProfiles').service('mainService', function($http) {
 
-	var data = [
-    {
-        "id": 0,
-        "first_name": "george",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
-    },
-    {
-        "id": 1,
-        "first_name": "lucille",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
-    },
-    {
-        "id": 2,
-        "first_name": "oscar",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
+    var user = {
+        username: 'Steven', age: 25, height: 72
     }
-  ];
 
   this.getUsers = function() {
-    return data;
+    return $http({
+        method: 'get', //get, post, delete, put
+        url: 'https://reqres.in/api/users?page=1' // the url part is the endpoint
+    })
   };
+
+  this.addUser = function(user) {
+    return $http({
+        method: 'post',
+        url: 'https://reqres.in/api/users',
+        data: user
+    });
+  }
+
+  this.removeFriend = function() {
+    return $http({
+        method: 'delete',
+        url: 'https://reqres.in/api/user' + user
+    })
+  }
 
 
 });
